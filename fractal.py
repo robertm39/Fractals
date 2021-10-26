d00d26c98219188188103879eeb820c7ded67bec
robertm39 <robertpmorton39@gmail.com>
2021-10-26 13:46:04 -0400

    added light side

--- fractal.py
+++ fractal.py
@@ -47,7 +47,25 @@ class RegionBounds:
         ix = round((x - self.min_x) / x_disp)
         iy = round((y - self.min_y) / y_disp)
         
-        return ix, iy
+        return int(ix), int(iy)
+    
+    def in_image_bounds(self, ix, iy, image=None):
+        if image is None:
+            image = self.image
+        
+        if ix < 0 or iy < 0:
+            return False
+        
+        width, height = image.size
+        
+        if ix >= width or iy >= height:
+            return False
+        
+        return True
+
+def square_norm(z):
+    x, y = np.real(z), np.imag(z)
+    return x*x + y*y
 
 def mandelbrot_proto():
     image_size = 600
@@ -66,18 +84,16 @@ def mandelbrot_proto():
     width, height = image.size
     max_iters = 129
     
-    # x_disp = (max_x - min_x) / (width - 1)
-    # y_disp = (max_y - min_y) / (height - 1)
     bounds = RegionBounds(min_x, max_x, min_y, max_y, image)
     
+    num_times = 0
+    
     for ix in range(width):
-        if (ix + 1) % 10 == 0:
+        if (ix + 1) % 50 == 0:
             print('processing column {}'.format(ix+1))
         
         for iy in range(height):
             # Get the complex number
-            # x = min_x + ix * x_disp
-            # y = min_y + iy * y_disp
             x, y = bounds.coords_in_graph(ix, iy)
             
             c = np.cdouble(x + y * 1.j)
@@ -89,6 +105,7 @@ def mandelbrot_proto():
             # Do the iterations
             for i in range(1, max_iters + 1):
                 z = z*z + c
+                num_times += 1
                 
                 # We got outside of the critical circle
                 if np.linalg.norm(z) > 2:
@@ -102,6 +119,91 @@ def mandelbrot_proto():
             else:
                 pixels[ix, iy] = (0, 0, 0)
     
+    print('times: {}'.format(num_times))
+    display(image)
+
+def mandelbrot_light_side():
+    """
+    Make the "light side" image.
+    """
+    image_size = 600
+    # Get a black image
+    image = Image.new('RGB', (image_size, image_size), 'black')
+    
+    # Get the pixel map
+    pixels = image.load()
+    
+    min_x = -2
+    max_x = 2
+    
+    min_y = -2
+    max_y = 2
+    
+    width, height = image.size
+    max_iters = 200
+    
+    bounds = RegionBounds(min_x, max_x, min_y, max_y, image)
+    
+    # Keep track of how many times each coordinate is hit
+    times_hit = np.zeros((image_size, image_size), dtype=np.int32)
+    
+    times = 0
+    
+    for ix in range(width):
+        if (ix + 1) % 50 == 0:
+            print('processing column {}'.format(ix+1))
+        
+        for iy in range(height):
+            x, y = bounds.coords_in_graph(ix, iy)
+            
+            c = np.cdouble(x + y * 1.j)
+            
+            # Get the starting z value
+            z = np.cdouble(0.0)
+            
+            diverges = False
+            values = list()
+            # Do the iterations
+            for i in range(1, max_iters + 1):
+                z = z*z + c
+                # crit_1 = np.linalg.norm(z) > 2
+                
+                # prev = z.copy()
+                values.append(z)
+                # assert prev == z
+                
+                # crit_2 = np.linalg.norm(z) > 2
+                
+                # assert crit_1 == crit_2
+                
+                times += 1
+                
+                # We got outside of the critical circle
+                # if np.linalg.norm(z) > 2:
+                if square_norm(z) > 4:
+                    diverges = True
+                    break
+            
+            if diverges:
+                for value in values:
+                    x, y = np.real(value), np.imag(value)
+                    ixv, iyv = bounds.coords_in_image(x, y)
+                    if bounds.in_image_bounds(ixv, iyv):
+                        times_hit[ixv, iyv] += 1
+    
+    # Put the values from times_hit into the image
+    
+    
+    print('times: {}'.format(times))
+    
+    max_hit = np.amax(times_hit)
+    for ix in range(width):
+        for iy in range(height):
+            intensity = times_hit[ix, iy] / max_hit
+            intensity = int(round(intensity * 255))
+            
+            pixels[ix, iy] = (intensity, intensity, intensity)
+    
     display(image)
 
 def region_bounds_test():
@@ -113,7 +215,8 @@ def region_bounds_test():
     print(region_bounds.coords_in_image(image, x, y))
 
 def main():
-    mandelbrot_proto()
+    # mandelbrot_proto()
+    mandelbrot_light_side()
     # region_bounds_test()
 
 if __name__ == '__main__':
